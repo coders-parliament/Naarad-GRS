be081350f570d85a9cc5c8bc2de0dab3b9f36f23
Saket Pathak <saketpathakitsme@gmail.com>
2026-05-07 07:10:08 +0530

    design authentication ui

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,16 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
     return {"message": "Naarad-GRS Backend Running"}
